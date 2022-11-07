cde16eb41af775aed2b05e930b7f29ab79cab375
QuantZero <yigitkasapoglu95@gmail.com>
2022-11-06 20:18:54 -0800

    replacing HTML with inline VueJS methods

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+var todos = [
+    {
+        text: `Learn HTML, CSS and Javascript`,
+        done: true
+    },
+    {
+        text: `Learn the basics of Vue JS` ,
+        done: false
+    }
+];
+
+const todosApp = {
+    data() {
+        return {
+            todos: window.todos,
+            newTodo: {
+                done: false
+            }
+        }
+    }
+};
+Vue.createApp(todosApp).mount(`#app`);
